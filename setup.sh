e737727a046ab9ff45b1f1c47117695b31edd219
M4r0s1 <74679791+M4r0s1@users.noreply.github.com>
2022-04-17 21:17:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,17 +58,32 @@ i3(){
     
     sudo apt upgrade
 
-    mkdir $HOME/repositoriesgit
 
-    cd $HOME/repositoriesgit
-
-    git clone https://github.com/M4r0s1/I3.git
-
-    cd I3
-
-    chmod +x setup.sh
-    
-    ./setup.sh
+	sudo apt-get install figlet
+	
+	figlet Se esta instalando i3
+	
+	sudo apt-get install i3
+	
+	sudo apt-get install polybar
+	
+	figlet Se esta configurando
+	
+	git clone https://github.com/M4r0s1/I3.git
+	
+	cd I3
+	
+	mkdir $HOME/.i3
+	
+	mkdir $HOME/.i3/rofithemes
+	
+	mkdir $HOME/.i3/wallpapers
+	
+	figlet Se estan pasando archivos
+	
+	mv 01.png $HOME/.i3/wallpapers
+	
+	mv arthur.rasi $HOME/.i3/rofithemes
 
 }
 
